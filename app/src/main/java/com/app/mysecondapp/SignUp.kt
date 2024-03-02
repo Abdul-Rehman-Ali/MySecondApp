053d23c869e43db674d3ca04cc1b939128300b28
Muhammad Jamil <jamil138.amin@gmail.com>
2024-03-02 08:36:48 +0500

    Responsive Views

---
+++ app/src/main/java/com/app/mysecondapp/SignUp.kt
@@ -0,0 +1,15 @@
+package com.app.mysecondapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+    }
+}
