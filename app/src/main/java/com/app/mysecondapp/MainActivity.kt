2d83b2efe281d832fee562c6d83684ed5d96fc68
Muhammad Jamil <jamil138.amin@gmail.com>
2024-03-02 09:18:23 +0500

    Responsive Views

--- app/src/main/java/com/app/mysecondapp/MainActivity.kt
+++ app/src/main/java/com/app/mysecondapp/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.app.mysecondapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show()
 
     }
 }
--- app/src/main/java/com/app/mysecondapp/SignUp.kt
+++ app/src/main/java/com/app/mysecondapp/SignUp.kt
@@ -1,10 +1,7 @@
 package com.app.mysecondapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
